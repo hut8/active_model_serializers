f3bca94ed84f402978f2167b44543e0a0d9264e4
Liam Bowen <liambowen@gmail.com>
2015-10-25 01:48:25 +0000

    Mostly taken from @jpaas, also fake rails context

---
+++ lib/grape/formatters/active_model_serializers.rb
@@ -0,0 +1,22 @@
+# A grape response formatter that can be used as 'formatter :json, Grape::Formatters::ActiveModelSerializers'
+#
+# Serializer options can be passed as a hash from your grape endpoint using env[:active_model_serializer_options],
+# or better yet user the render helper in Grape::Helpers::ActiveModelSerializers
+module Grape
+  module Formatters
+    module ActiveModelSerializers
+      RequestContext = Struct.new(:original_url, :query_parameters)
+
+      class << self
+        def call(resource, env)
+          serializer_options = {}
+          serializer_options.merge!(env[:active_model_serializer_options]) if env[:active_model_serializer_options]
+
+          serializer_options[:context] = RequestContext.new(env['REQUEST_URI'],
+                                                            env['rack.request.query_hash'])
+          ActiveModel::SerializableResource.new(resource, serializer_options).to_json(serializer_options)
+        end
+      end
+    end
+  end
+end
